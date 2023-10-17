2aae48b01db90f63784eaea97b65196da70ed01a
SneakyNinja2041 <135566030+SneakyNinja2041@users.noreply.github.com>
2023-10-18 08:49:24 +1100

    added different enemy prefabs
    added sprites for both an enemy, and the player character

---
+++ Assets/Assets/Scripts/CharStats.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+
+    public int health;
+    public int atk;
+    public int level;
+
+    public ParticleSystem attackPS;
+
+    void Start()
+    {
+        
+    }
+
+
+}
